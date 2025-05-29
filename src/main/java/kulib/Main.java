0a25686ba37c27e0c5d45b93ee3e8fc9f4b3c768
kulib <1615279202@qq.com>
2025-05-29 09:45:24 +0800

    修复了获取间隔过久的bug

--- src/main/java/kulib/Main.java
+++ src/main/java/kulib/Main.java
@@ -6,14 +6,13 @@ public class Main {
     while (true) {
         try {
             main_prosess.api_main();
-            // 你的主逻辑
         } catch (Exception e) {
             System.err.println("异常捕获：程序继续运行 -> " + e.getMessage());
+            try {
+                Thread.sleep(3000); // 等待几秒再试
+            } 
+            catch (InterruptedException ignored) {}
         }
-
-        try {
-            Thread.sleep(3000); // 等待几秒再试
-        } catch (InterruptedException ignored) {}
     }
 }
 
